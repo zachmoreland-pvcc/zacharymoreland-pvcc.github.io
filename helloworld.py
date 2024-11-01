2a5476ce695506645eeb25d0577e80b5c8de0fde
zacharymoreland-pvcc.github.io <zbm67992@email.vccs.edu>
2024-10-31 20:26:06 -0400

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+# Name: Zach Moreland
+#Prog Purpose: Hello World
+
+print("Hello World! This is my first Python Program.")
+print("I can Program in HTML, which is awesome.")
+print("However, HTML cannot do arithmetic.")
+print("But Python can: \n\t Add \n\t Subtract \n\t Multiply \n\t Divide")
+print("and lots of other things!")
